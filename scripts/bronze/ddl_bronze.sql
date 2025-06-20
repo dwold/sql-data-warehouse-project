06f25adf841ce6d0e26cd417f7e4f0b7edbf36be
Dagnachew Woldeyohannes <54189672+dwold@users.noreply.github.com>
2025-06-20 10:01:08 -0700

    Update ddl_bronze.sql

--- scripts/bronze/ddl_bronze.sql
+++ scripts/bronze/ddl_bronze.sql
@@ -7,8 +7,8 @@ DDL Script: Create Bronze Tables
 
  Purpose       : This script creates tables in the 'bronze' schema, dropping 
 				 existing tables if they already exist.
-				 Run this script to re-define the DDL structure of 'bronze'
-				 Tables
+				 Run this script to re-define the DDL structure
+				 of 'bronze' Tables.
  
 Author        : Dagnachew Woldeyohannes
  
